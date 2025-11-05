5b02c542dfedf452d3f1f4043b7455167551de3d
Harrison owede <harrisonowede3@gmail.com>
2025-11-05 15:01:08 +0100

    Fixing redis url

--- src/jobs/clipQueue.js
+++ src/jobs/clipQueue.js
@@ -1,4 +1,4 @@
 // src/jobs/clipQueue.js
 import Queue from "bull";
 
-export const clipQueue = new Queue("clipQueue", process.env.REDIS_URL || "redis://127.0.0.1:6379");
+export const clipQueue = new Queue("clipQueue", process.env.REDIS_URL);
