7fc847cd901dec49c13e02a57fdb09a76735ff37
Harrison owede <harrisonowede3@gmail.com>
2025-11-17 17:02:22 +0100

    still trying

--- src/auth/twitch.js
+++ src/auth/twitch.js
@@ -1,9 +1,9 @@
 import { ApiClient } from "@twurple/api";
-import { ClientCredentialsAuthProvider } from "@twurple/auth";
+import { AppTokenAuthProvider } from "@twurple/auth";
 
 const clientId = process.env.TWITCH_CLIENT_ID;
 const clientSecret = process.env.TWITCH_CLIENT_SECRET;
 
-const authProvider = new ClientCredentialsAuthProvider(clientId, clientSecret);
+const authProvider = new AppTokenAuthProvider(clientId, clientSecret);
 
 export const apiClient = new ApiClient({ authProvider });
